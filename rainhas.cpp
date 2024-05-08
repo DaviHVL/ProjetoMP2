80169ea11706bb3946232518dbb446bf0fd981f7
Seu Nome <davihvl@gmail.com>
2024-05-08 16:18:46 -0300

    Commit 6

--- rainhas.cpp
+++ rainhas.cpp
@@ -13,15 +13,33 @@ bool verificador_caracteres(string tabuleiro) {
 }
 
 /*
-Função para verificar se há 64 caracteres.
+Implementação da função para verificar se há 64 caracteres.
 */
 bool verificador_numeros(string tabuleiro){
-    if (tabuleiro.length() == 64){
+    if (tabuleiros.length() == 64){
         return true;
     }
     else{
         return false;
     }
+}
+
+/*
+Implementação da função para verificar se há apenas uma 1 rainha em cada linha do tabuleiro.
+*/
+bool verificador_umaRainha(string tabuleiro){
+    for (size_t i=0; i<64; i+=8){
+            int contad = 0;
+        for (size_t z=i; z<(i+8); z++){
+            if tabuleiro[z]=='1'{
+                contad++;
+            }
+        }
+        if contad!=1{
+            return false;
+        }
+    }
+    return true;
 }
 
 /*
@@ -32,6 +50,9 @@ int resposta_rainhas(string tabuleiro) {
         return -1;
     }
     if (verificador_numeros(tabuleiro) == false) {
+        return -1;
+    }
+    if (verificador_umaRainha(tabuleiro) == false) {
         return -1;
     }
     return 0;
--- rainhas.hpp
+++ rainhas.hpp
@@ -20,5 +20,10 @@ Função para verificar se há 64 caracteres.
 */
 bool verificador_numeros(string tabuleiro);
 
+/*
+Função para verificar se há apenas uma 1 rainha em cada linha do tabuleiro.
+*/
+bool verificador_umaRainha(string tabuleiro);
+
 #endif   // RAINHAS_H_INCLUDED
 
