c8f5eb32b33e3cd354be6c18f02e24e667997aee
Seu Nome <davihvl@gmail.com>
2024-05-08 16:01:31 -0300

    Commit 1

---
+++ rainhas.hpp
@@ -0,0 +1,13 @@
+#ifndef RAINHAS_H_INCLUDED
+#define RAINHAS_H_INCLUDED
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+Função Principal.
+*/
+int resposta_rainhas(string tabuleiro);
+
+#endif   // RAINHAS_H_INCLUDED
